9a6dcd47a5229edb32bbb9234bd368b29386c954
liaozuliang <lzlhen1988@126.com>
2017-12-20 20:35:20 +0800

    新增配置：日志打印mybatis sql

---
+++ src/main/java/com/liaozl/service/TestService.java
@@ -0,0 +1,15 @@
+package com.liaozl.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author liaozuliang
+ * @date 2017-12-20
+ */
+@Service
+public class TestService {
+
+    public void test() {
+        System.out.println(123);
+    }
+}
